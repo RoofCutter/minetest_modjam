9b932d5f9a52141245b9232b6eaaaf1df9cbef68
RoofCutter <roofcutter@pm.me>
2018-10-09 18:26:22 -0400

    Added badideas mod

---
+++ mods/badideas/init.lua
@@ -0,0 +1,7 @@
+print("Loaded bad ideas mod")
+
+-- Cactus Snowball
+minetest.register_tool("badideas:cactus_snowball", {
+        description = "Cactus Snowball",
+        inventory_image = "badideas_cactus_snowball.png",
+    })
