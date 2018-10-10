2d81923bef261bbb8f84a4942e9ef879af9bf987
RoofCutter <roofcutter@pm.me>
2018-10-09 22:06:21 -0400

    Remove excessive debug text

--- mods/badideas/projectile_collision.lua
+++ mods/badideas/projectile_collision.lua
@@ -1,7 +1,6 @@
 -- Insert projectile collision code here
 function projectile_collision(self)
     -- Destroy the projectile if it is no longer falling
-    print(self.object:get_velocity()["y"])
     if self.object:get_velocity()["y"] == 0 and self.object:get_acceleration()["y"] < 0 then
         self.object:remove()
     end
