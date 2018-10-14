87686a9f4fdee7255f1f3f4e659388a91b53174b
RoofCutter <roofcutter@pm.me>
2018-10-13 23:56:26 -0400

    Beefsteak now increases beefsteak stat

--- mods/brightarts/mana/beefsteak.lua
+++ mods/brightarts/mana/beefsteak.lua
@@ -1,6 +1,22 @@
+local function beefsteak_consume(itemstack, user)
+    local current_beefsteak = user:get_attribute("beefsteak")
+    -- Set the current_beefsteak to 0 if get_attribute() returns nil
+    if not current_beefsteak then
+        current_beefsteak = 0
+    end
+    current_beefsteak = current_beefsteak + 1
+    user:set_attribute("beefsteak", current_beefsteak)
+    minetest.chat_send_player(user:get_player_name(), "Beefsteak: " .. current_beefsteak)
+    -- Heal
+    user:set_hp(user:get_hp() + 8, "Eating beefsteak")
+    itemstack:take_item()
+    return itemstack
+end
+
 -- Add beefsteak
 minetest.register_craftitem("brightarts:beefsteak", {
     description = "Beefsteak",
     inventory_image = "beefsteak.png",
-    on_use = minetest.item_eat(8),
+    on_use = beefsteak_consume,
 })
+
