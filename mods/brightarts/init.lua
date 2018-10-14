dd98c0436bf62c999d137526cef29c7f7bacf5c5
Collin McKinley <bravotic@aaathats3as.com>
2018-10-13 23:07:21 -0400

    fuck my life

--- mods/brightarts/init.lua
+++ mods/brightarts/init.lua
@@ -2,6 +2,10 @@
 local default_path = minetest.get_modpath("brightarts")
 dofile(default_path .. "/mana/beefsteak.lua")
 
+local default_path = minetest.get_modpath("brightarts")
+dofile(default_path .. "/metal/metal.lua")
+
+
 minetest.register_craftitem("brightarts:help", {
 	wield_image = "battery.pngte",
   inventory_image = "battery.png",
