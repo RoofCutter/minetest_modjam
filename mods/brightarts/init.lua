132e9824986bfef878a805b956555fda2fa3f93b
Collin McKinley <bravotic@aaathats3as.com>
2018-10-13 21:43:35 -0400

    BrightArts: fixed crash faggots

--- mods/brightarts/init.lua
+++ mods/brightarts/init.lua
@@ -5,13 +5,3 @@ minetest.register_craftitem("brightarts:help", {
   description = "A full battery"
 })
 
-local player = minetest.get_player_by_name("username")
-
-local idx = player:hud_add({
-         hud_elem_type = "text",
-         position = {x = 1, y = 0},
-         offset = {x=-100, y = 20},
-         scale = {x = 100, y = 100},
-         text = "My Text"
-})
-
